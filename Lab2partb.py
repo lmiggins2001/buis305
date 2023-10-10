0806cafc6ac0a695f4a04d148c0f093d3546da2f
lab2 <migginsl0314@gmail.com>
2023-10-10 15:38:22 -0400

    lab 2files

---
+++ Lab2partb.py
@@ -0,0 +1,53 @@
+number1 = 10
+number2 = 11
+number3 = 12
+number4 = 13
+
+
+add = number1 + number2 + number3 + number4
+
+
+print("The sum is:", add)
+mult=number1*number2*number3*number4
+print('Equals:',mult)
+sub= number4-number3-number2-number1
+print('Equals:',sub)
+division= (number4/number3)
+
+try:
+    result = number4 / number3
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Division by zero is not allowed.")
+
+number1=11
+number2=10
+number3=13
+number4=12
+add = number1 + number2 + number3 + number4
+print("The sum is:", add)
+mult=number1*number2*number3*number4
+print('Answer equals',mult)
+subtract=number4-number3-number2-number1
+print('Answer equals',subtract)
+division= (number4/number3)
+
+try:
+    result = number4 / number3
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Division by zero is not allowed.")
+
+add= number1 + number2
+print("The sum is:", add)
+mult=number3*number4
+print('Answer equals',mult)
+subtract= number1-number2
+print('Answer equals',subtract)
+division= (number1/number2)
+
+try:
+    result = number4 / number3
+    print("Result:", result)
+except ZeroDivisionError:
+    print("Division by zero is not allowed.")
