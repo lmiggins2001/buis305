ece60d0f0f4e4dd946fdeebbc9ac1135db3b6c93
lab2 <migginsl0314@gmail.com>
2023-10-17 20:36:23 -0400

    lab 3files

---
+++ Lab3.py
@@ -0,0 +1,102 @@
+number1= input ('Enter number 1')
+number2= input ('Enter number 2')
+print(number1,number2)
+
+
+num1 =input("Enter the first number: ")
+num2 =input("Enter the second number: ")
+sum = int(num1) + int(num2)
+print('The sum of num1 and num2 is',sum)
+num1 =input("Enter the first number: ")
+num2 =input("Enter the second number: ")
+
+
+difference = int(num1) - int(num2)
+multiplication = int(num1) * int(num2)
+division = int(num1) / int(num2)
+
+
+print("Difference:", difference)
+print("Multiplication:", multiplication)
+print("Division:", division)
+
+
+
+
+num1 =input("Enter the first number: ")
+num2 =input("Enter the second number: ")
+num3 =input("Enter the third number: ")
+
+
+sum=int(num1)+int(number2)+int(num3)
+
+
+difference= int(number1)-int(number2)-int(num3)
+
+
+multiplication= int(num1)*int(num2)*int(num3)
+
+
+division=int(num1)/int(num2)/int(num3)
+print("Difference:", difference)
+print("Multiplication:", multiplication)
+print("Division:", division)
+import math
+# Return the value of 9 raised to the power of 3
+result = math.pow(9, 3)
+print(result)
+
+# Import math Library
+import math
+# Return the cosine of different numbers
+print (math.cos(-5.65))
+
+# Import math Library
+import math
+# Convert from radians to degrees:
+print (math.degrees(-40))
+
+# Import math Library
+import math
+# Initialize the number of items to choose from
+n = 9
+# Initialize the number of possibilities to choose
+k = 2
+# Print total number of possible combinations
+print (math.comb(n, k))
+
+#Import math Library
+import math
+#Print absolute values from numbers
+print(math.fabs(-50))
+
+# Import math Library
+import math
+# Return the arc sine of numbers
+print(math.asin(0.67))
+
+#Import math Library
+import math
+#Return factorial of a number
+print(math.factorial(3))
+
+# Import math library
+import math
+# Round numbers down to the nearest integer
+print(math.floor(23.5))
+
+# Import math Library
+import math
+# Print error function for different numbers
+print (math.erf(0.56))
+
+# Import math Library
+import math
+# Return the sine of different values
+print (math.sin(2.23))
+
+# Import math Library
+import math
+# Return the square root of different numbers
+print (math.sqrt(64))
+
